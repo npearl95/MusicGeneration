c575068e2c48bd44f74f81ec40276d050746caca
Chau <npearl1995@gmail.com>
2019-03-18 18:18:30 -0400

    Google login and Select parameters work

---
+++ app/src/main/java/edu/temple/musicgen/GenerateSong.java
@@ -0,0 +1,57 @@
+package edu.temple.musicgen;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class GenerateSong extends AppCompatActivity {
+
+    private static final String TAG = "SignInActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_song);
+
+        Intent myIntent = getIntent();
+        // Views
+        TextView user_name = (TextView) findViewById(R.id.user_name);
+        String email = myIntent.getStringExtra("email");
+        user_name.setText(email);
+
+
+        //Spinner Genre
+        Spinner spinnergenre = (Spinner) findViewById(R.id.genre_spinner);
+        ArrayAdapter<CharSequence> genreAdapter = ArrayAdapter.createFromResource(this, R.array.genre, android.R.layout.simple_spinner_item);
+        genreAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnergenre.setAdapter(genreAdapter);
+        //Spinner Tempo
+        Spinner spinnerTempo = (Spinner) findViewById(R.id.tempo_spinner);
+        ArrayAdapter<CharSequence> tempoAdapter = ArrayAdapter.createFromResource(this, R.array.tempo, android.R.layout.simple_spinner_item);
+        tempoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerTempo.setAdapter(tempoAdapter);
+        //Spinner Duration
+        Spinner spinnerDuration = (Spinner) findViewById(R.id.duration_spinner);
+        ArrayAdapter<CharSequence> durationAdapter = ArrayAdapter.createFromResource(this, R.array.duration, android.R.layout.simple_spinner_item);
+        durationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDuration.setAdapter(durationAdapter);
+
+
+    }
+}
